d7961184d454da433202d8e1568fd2f5a5faa5cd
emirhan <emirhan@duck.com>
2024-11-27 22:57:03 +0300

    Refactor app.js to update routing and project control initialization

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
+// Variables
 const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./src/Modules/Database/db');
-const hospitalRoutes = require('./src/Routes/Hospitals');
-
 const app = express();
-app.use(express.json());
-app.use('/hospitals', hospitalRoutes);
-
+const Project = require('./src/Modules/Functions/Control');
+const router = require('./src/Routes/');
 const PORT = process.env.PORT || 3000;
+
+// Control Projects Variables
+Project.requireCheck();
+
+// App Config
+app.use(express.json());
+app.use('/api', router);
+
 app.listen(PORT, () => {
-    console.log(`System active on: http://localhost:${PORT}`);
+    console.log(`Backend on: http://${process.env.HOST}:${PORT}`);
 });
