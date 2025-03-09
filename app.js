1b1ab20f80af842a1149190c72cd987abfc9bf4f
Hasan Böcek <bocekhasan@icloud.com>
2025-03-09 23:09:10 +0300

    Add swagger & configure

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ const app = express();
 const Project = require('./src/Modules/Functions/Control');
 const router = require('./src/Routes/');
 const PORT = process.env.PORT || 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Hospital Management API',
+        version: '1.0.0',
+        description: 'Hospital Management API',
+      },
+      servers: [
+        {
+          url: `http://localhost:${PORT}`,
+        },
+      ],
+    },
+    apis: ['./src/Routes/*.js'],
+  };
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 // Control Projects Variables
 Project.requireCheck();
 
 // App Config
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api', router);
 
 app.listen(PORT, () => {
