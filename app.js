325e2f9ee8e731e1eda5ce6c55e8e1bcbe3a0a7e
emirhan <emirhan@duck.com>
2025-05-10 19:59:01 +0300

    Add CORS middleware to enable cross-origin requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 // Variables
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const Project = require('./src/Modules/Functions/Control');
 const router = require('./src/Routes/');
 const PORT = process.env.PORT || 3000;
@@ -32,6 +33,11 @@ Project.requireCheck();
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api', router);
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.listen(PORT, () => {
     console.log(`Backend on: http://${process.env.HOST}:${PORT}`);
